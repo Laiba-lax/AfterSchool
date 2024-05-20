b3034da6425882afb3ebb62490364e7f223f0f34
Laiba-lax <laiba.laeck.25@gmail.com>
2024-05-20 16:47:49 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from './components/shared-components/NavbarComponent';
+import Hero from './components/Hero';
+import { Question } from './components/Question';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavbarComponent/>  
+      <Hero/>
+      <Question/>
     </div>
   );
 }
